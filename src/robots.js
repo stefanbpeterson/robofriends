3584eb4df9954a3a7d52027d664cb48a17852397
stefanbpeterson <stefanbpeterson@gmail.com>
2020-03-03 07:36:04 -0700

    Lots of changes -- creating the first cards

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Tyrone Clader',
+    username: 'Big Rone',
+    email: 'big.rone@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Brant Roble',
+    username: 'Kick Socks',
+    email: 'kicksocks@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Jermey James',
+    username: 'Jerm',
+    email: 'callmejerm@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Mick Morphin',
+    username: 'Lil Mick',
+    email: 'mick.dick@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'Dave Pimm',
+    username: 'Jailbird',
+    email: 'pimm4real@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'Katarina Marbel',
+    username: 'Marbelgirl',
+    email: 'marbel.girl@gmail.com'
+  },
+  {
+    id: 7,
+    name: 'Jack Vegas',
+    username: 'Carson',
+    email: 'jackvegas@gmail.com'
+  },
+  {
+    id: 8,
+    name: 'Tilly Whistle',
+    username: 'Whist',
+    email: 'whist@gmail.com'
+  },
+  {
+    id: 9,
+    name: 'Cheesepants',
+    username: 'Cheesepants',
+    email: 'cheesepants@gmail.com'
+  },
+  {
+    id: 10,
+    name: 'Evangeline Quinn',
+    username: 'Eva',
+    email: 'evaqp@gmail.com'
+  }
+]
